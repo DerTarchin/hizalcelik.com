020b877f7b6aef3ff3f97dfbf4a16701db3d367a
Hizal Celik <hizalcelik@gmail.com>
2019-04-01 23:32:20 -0400

    added singularity p1

---
+++ assets/js/indie/singularity.js
@@ -0,0 +1,190 @@
+var img_data = [
+  [
+    {
+      img: 'bts.jpg',
+      size: '2000x1250',
+      title: 'Workspace',
+      cap: 'Mandelbulb3D user interface with one of my scenes.'
+    }
+  ],
+  [
+    {
+      img: 'fractal1.jpg',
+      size: '2000x2000',
+      title: 'Sample Fractal',
+      cap: 'Using default equation parameters and variables.'
+    }
+  ],
+  [
+    {
+      img: 'comparison.jpg',
+      size: '2000x800',
+      title: 'Before / After (small) / After (large)',
+      cap: 'Left is original lighting and color, middle is made to look like a circuit board, and right to look like a city.'
+    }
+  ],
+  [
+    {
+      img: 'world9.jpg',
+      size: '2000x834',
+      title: '',
+      cap: ''
+    },
+    {
+      img: 'world10.jpg',
+      size: '2000x1250',
+      title: '',
+      cap: ''
+    },
+    {
+      img: 'world2.jpg',
+      size: '1920x800',
+      title: '',
+      cap: ''
+    },
+    {
+      img: 'world3.jpg',
+      size: '1920x800',
+      title: '',
+      cap: ''
+    },
+    {
+      img: 'world4.jpg',
+      size: '1920x800',
+      title: '',
+      cap: ''
+    },
+    // {
+    //   img: 'world5.jpg',
+    //   size: '1920x800',
+    //   title: '',
+    //   cap: ''
+    // },
+    {
+      img: 'world6.jpg',
+      size: '1920x800',
+      title: '',
+      cap: ''
+    },
+    {
+      img: 'world7.jpg',
+      size: '1920x800',
+      title: '',
+      cap: ''
+    },
+    {
+      img: 'world8.jpg',
+      size: '1920x800',
+      title: '',
+      cap: ''
+    }
+  ],
+  [
+    {
+      img: 'city1.jpg',
+      size: '1920x800',
+      title: '',
+      cap: ''
+    },
+    {
+      img: 'city2.jpg',
+      size: '2000x834',
+      title: '',
+      cap: ''
+    },
+    {
+      img: 'city3.jpg',
+      size: '1920x800',
+      title: '',
+      cap: ''
+    },
+    {
+      img: 'city4.jpg',
+      size: '1920x800',
+      title: '',
+      cap: ''
+    },
+    {
+      img: 'city5.jpg',
+      size: '1920x800',
+      title: '',
+      cap: ''
+    },
+    {
+      img: 'city6.jpg',
+      size: '1920x800',
+      title: '',
+      cap: ''
+    },
+    {
+      img: 'city7.jpg',
+      size: '1920x800',
+      title: '',
+      cap: ''
+    }
+  ],
+  [
+    {
+      img: 'abstract5.jpg',
+      size: '2000x1146',
+      title: '',
+      cap: ''
+    },
+    {
+      img: 'abstract9.jpg',
+      size: '1920x800',
+      title: '',
+      cap: ''
+    },
+    {
+      img: 'abstract1.jpg',
+      size: '1920x800',
+      title: '',
+      cap: ''
+    },
+    // {
+    //   img: 'abstract2.jpg',
+    //   size: '1920x800',
+    //   title: '',
+    //   cap: ''
+    // },
+    {
+      img: 'abstract3.jpg',
+      size: '1920x800',
+      title: '',
+      cap: ''
+    },
+    // {
+    //   img: 'abstract4.jpg',
+    //   size: '2000x1125',
+    //   title: '',
+    //   cap: ''
+    // },
+    {
+      img: 'abstract6.jpg',
+      size: '1920x800',
+      title: '',
+      cap: ''
+    },
+    {
+      img: 'abstract7.jpg',
+      size: '1920x800',
+      title: '',
+      cap: ''
+    },
+    {
+      img: 'abstract8.jpg',
+      size: '1920x800',
+      title: '',
+      cap: ''
+    },
+    {
+      img: 'abstract10.jpg',
+      size: '1920x800',
+      title: '',
+      cap: ''
+    }
+  ]
+];
+
+initIndiePage(img_data, 'singularity');
