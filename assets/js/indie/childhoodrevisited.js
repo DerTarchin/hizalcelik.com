9e8e67ce54403f5d6a112038fa932fd945e19561
Hizal Celik <hizalcelik@gmail.com>
2020-02-02 23:13:31 -0500

    added childhood revisted page

---
+++ assets/js/indie/childhoodrevisited.js
@@ -0,0 +1,104 @@
+var img_data = [
+  [
+    {
+      img: 'boat.jpg',
+      size: '2000x1333',
+    },
+    {
+      img: 'boat_orig.jpg',
+      size: '2000x1333',
+    },
+  ],
+  [
+    {
+      img: 'cake.jpg',
+      size: '2000x1333',
+    },
+    {
+      img: 'cake_orig.jpg',
+      size: '2000x1333',
+    },
+  ],
+  [
+    {
+      img: 'chicken.jpg',
+      size: '1333x2000',
+    },
+    {
+      img: 'chicken_orig.jpg',
+      size: '1333x2000',
+    },
+  ],
+  [
+    {
+      img: 'dino.jpg',
+      size: '1333x2000',
+    },
+    {
+      img: 'dino_orig.jpg',
+      size: '1333x2000',
+    },
+  ],
+  [
+    {
+      img: 'dollhouse.jpg',
+      size: '2000x1333',
+    },
+    {
+      img: 'dollhouse_orig.jpg',
+      size: '2000x1333',
+    },
+  ],
+  [
+    {
+      img: 'fence.jpg',
+      size: '1333x2000',
+    },
+    {
+      img: 'fence_orig.jpg',
+      size: '1333x2000',
+    },
+  ],
+  [
+    {
+      img: 'floor.jpg',
+      size: '2000x1333',
+    },
+    {
+      img: 'floor_orig.jpg',
+      size: '2000x1333',
+    },
+  ],
+  [
+    {
+      img: 'merry.jpg',
+      size: '2000x1333',
+    },
+    {
+      img: 'merry_orig.jpg',
+      size: '2000x1333',
+    },
+  ],
+  [
+    {
+      img: 'studio.jpg',
+      size: '1333x2000',
+    },
+    {
+      img: 'studio_orig.jpg',
+      size: '1333x2000',
+    },
+  ],
+  [
+    {
+      img: 'toys.jpg',
+      size: '2000x1333',
+    },
+    {
+      img: 'toys_orig.jpg',
+      size: '2000x1333',
+    },
+  ],
+];
+
+initIndiePage(img_data, 'childhoodrevisited');
