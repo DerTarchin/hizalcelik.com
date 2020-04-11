e0ce3c392020d0ec6a94871ade320fbc89713907
Hizal Celik <hizalcelik@gmail.com>
2020-04-10 20:19:05 -0400

    Added SEM page

---
+++ assets/js/indie/sem.js
@@ -0,0 +1,72 @@
+var img_data = [
+  [
+    {
+      img: 'banner.jpg',
+      size: '2000x559'
+    },
+  ],
+  [
+    {
+      img: 'final5.jpg',
+      size: '2000x1333'
+    },
+  ],
+  [
+    {
+      img: 'final2.jpg',
+      size: '2000x1333'
+    },
+  ],
+  [
+    {
+      img: 'final4.jpg',
+      size: '2000x1333'
+    },
+  ],
+  [
+    {
+      img: 'final3.jpg',
+      size: '2000x1333'
+    },
+  ],
+  [
+    {
+      img: 'final6.jpg',
+      size: '2000x1333'
+    },
+  ],
+  [
+    {
+      img: 'final1.jpg',
+      size: '2000x1333'
+    },
+  ],
+  [
+    {
+      img: 'orig1.jpg',
+      size: '1280x1024'
+    },
+    {
+      img: 'orig2.jpg',
+      size: '1280x1024'
+    },
+    {
+      img: 'orig3.jpg',
+      size: '1280x1024'
+    },
+    {
+      img: 'orig4.jpg',
+      size: '1280x1024'
+    },
+    {
+      img: 'orig5.jpg',
+      size: '1280x1024'
+    },
+    {
+      img: 'orig6.jpg',
+      size: '1280x1024'
+    },
+  ],
+];
+
+initIndiePage(img_data, 'sem');
