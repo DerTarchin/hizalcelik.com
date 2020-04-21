2806614b7f03e3814aaf1ff29a45e54d56c711b2
Hizal Celik <hizalcelik@gmail.com>
2020-04-21 18:51:28 -0400

    Added lineplotter page

---
+++ assets/js/indie/lineplotter.js
@@ -0,0 +1,144 @@
+var img_data = [
+  [
+    {
+      img: 'sketches.jpg',
+      size: '1494x2000',
+      title: 'Cycloid Circles',
+      cap: 'Drafting squares, then circles, in a looping animated pattern.'
+    },
+  ],
+  [
+    {
+      img: 'plotter.jpg',
+      size: '768x1024',
+      title: 'Line Plotter',
+      cap: '2D line plotter drawing my designs.'
+    },
+  ],
+  [
+    {
+      img: 'final1.JPG',
+      size: '2000x2000',
+      title: '-1.5+i',
+    },
+    {
+      img: 'final2.JPG',
+      size: '2000x2000',
+      title: '-0.5+(ix1.5)',
+    },
+  ],
+  [
+    {
+      img: '-0.3xi.jpg',
+      size: '1000x1000',
+      title: '-0.3xi'
+    },
+    {
+      img: '-0.5+(ix1.5).jpg',
+      size: '1000x1000',
+      title: '-0.5+(ix1.5)'
+    },
+    {
+      img: '-0.5+i.jpg',
+      size: '1000x1000',
+      title: '-0.5+i'
+    },
+    {
+      img: '-0.5xi.jpg',
+      size: '1000x1000',
+      title: '-0.5xi'
+    },
+    {
+      img: '-1.5+i.jpg',
+      size: '1000x1000',
+      title: '-1.5+i'
+    },
+    {
+      img: '-1.5xi.jpg',
+      size: '1000x1000',
+      title: '-1.5xi'
+    },
+    {
+      img: '-2+i.jpg',
+      size: '1000x1000',
+      title: '-2+i'
+    },
+    {
+      img: '-2xi.jpg',
+      size: '1000x1000',
+      title: '-2xi'
+    },
+    {
+      img: '0.5-(ix1.5).jpg',
+      size: '1000x1000',
+      title: '0.5-(ix1.5)'
+    },
+    {
+      img: '0.5xi.jpg',
+      size: '1000x1000',
+      title: '0.5xi'
+    },
+    {
+      img: '1.5+i.jpg',
+      size: '1000x1000',
+      title: '1.5+i'
+    },
+    {
+      img: '1.5xi.jpg',
+      size: '1000x1000',
+      title: '1.5xi'
+    },
+    {
+      img: '1+i.jpg',
+      size: '1000x1000',
+      title: '1+i'
+    },
+    {
+      img: '2+i.jpg',
+      size: '1000x1000',
+      title: '2+i'
+    },
+    {
+      img: '2xi.jpg',
+      size: '1000x1000',
+      title: '2xi'
+    },
+    {
+      img: 'div1.5_-0.5+i.jpg',
+      size: '1000x1000',
+      title: 'div1.5 -0.5+i'
+    },
+    {
+      img: 'div1.5_-ixi+i.jpg',
+      size: '1000x1000',
+      title: 'div1.5 -ixi+i'
+    },
+    {
+      img: 'div1.5_1.5xi.jpg',
+      size: '1000x1000',
+      title: 'div1.5 1.5xi'
+    },
+    {
+      img: 'div1.5_ixi+i.jpg',
+      size: '1000x1000',
+      title: 'div1.5 ixi+i'
+    },
+    {
+      img: 'div3_-0.5xi.jpg',
+      size: '1000x1000',
+      title: 'div3 -0.5xi'
+    },
+    {
+      img: 'div3_0.1xi.jpg',
+      size: '1000x1000',
+      title: 'div3 0.1xi'
+    },
+    {
+      img: 'div3_0.2xi.jpg',
+      size: '1000x1000',
+      title: 'div3 0.2xi'
+    },
+  ]
+];
+
+initIndiePage(img_data, 'lineplotter');
