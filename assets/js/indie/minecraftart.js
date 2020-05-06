d8df7ae200347254024fb0aca14119cacc9189b4
Hizal Celik <hizalcelik@gmail.com>
2020-05-06 09:09:18 -0400

    Adds minecraft art page

---
+++ assets/js/indie/minecraftart.js
@@ -0,0 +1,249 @@
+var img_data = [
+  [
+    {
+      img: 'cropcircles_banner.jpg',
+      size: '1920x707',
+      title: 'Crop Circles',
+      cap: 'Glowing abstract designs appearing on the ocean floor near a town.'
+    },
+  ],
+  [
+    {
+      img: 'cropcircle_idea.jpg',
+      size: '2000x1250',
+      title: 'Designing Crop Circles',
+    },
+  ],
+  [
+    {
+      img: 'exploring.jpg',
+      size: '1920x1080',
+      title: 'Location Scouting',
+      cap: 'A secluded town by the waterside worked the best for this piece.'
+    },
+    {
+      img: 'cropcircle_before.jpg',
+      size: '2000x1250',
+      title: 'Location Scouting',
+      cap: 'A secluded town by the waterside worked the best for this piece.'
+    },
+    {
+      img: 'cropcircle3.jpg',
+      size: '1920x1080',
+      title: 'Building the Circles',
+      cap: 'Crop circle carvings visible underwater, before adding glowstone.'
+    },
+    {
+      img: 'cropcircle2.jpg',
+      size: '1920x1080',
+      title: 'Building the Circles',
+      cap: 'Crop circle carvings visible underwater, before adding glowstone.'
+    },
+    {
+      img: 'cropcircle4.jpg',
+      size: '1920x1080',
+      title: 'Adding Glowstone',
+      cap: 'By adding glowstone, the designs are visible from the surface.'
+    },
+    {
+      img: 'cropcircle5.jpg',
+      size: '1920x1080',
+      title: 'Adding Glowstone',
+      cap: 'By adding glowstone, the designs are visible from the surface.'
+    },
+    {
+      img: 'cropcircle6.jpg',
+      size: '1920x1080',
+      title: 'View from Above',
+      cap: 'The final design as viewed from a cliffside vantage point.'
+    },
+  ],
+  [
+    {
+      img: 'wall_banner.jpg',
+      size: '1920x707',
+      title: 'The Wall',
+      cap: 'A giant curtain of water spanning the length of a canyon village.'
+    },
+  ],
+  [
+    {
+      img: 'wall_idea.jpg',
+      size: '2000x1125',
+      title: 'Designing the Wall',
+    },
+  ],
+  [
+    {
+      img: 'wall_before2.jpg',
+      size: '1920x1080',
+      title: 'Location Scouting',
+      cap: 'Trying to find the best location to install a curtain of water.'
+    },
+    {
+      img: 'wall_before3.jpg',
+      size: '1920x1080',
+      title: 'Location Scouting',
+      cap: 'Trying to find the best location to install a curtain of water.'
+    },
+    {
+      img: 'wall_before4.jpg',
+      size: '1920x1080',
+      title: 'Location Scouting',
+      cap: 'Trying to find the best location to install a curtain of water.'
+    },
+    {
+      img: 'wall1.jpg',
+      size: '1920x1080',
+      title: 'Final Installation',
+      cap: 'A curtain of water in a grand scale.'
+    },
+    {
+      img: 'wall2.jpg',
+      size: '1920x1080',
+      title: 'Final Installation',
+      cap: 'A curtain of water in a grand scale.'
+    },
+    {
+      img: 'wall3.jpg',
+      size: '1920x1080',
+      title: 'Final Installation',
+      cap: 'A curtain of water in a grand scale.'
+    },
+    {
+      img: 'wall4.jpg',
+      size: '1920x1080',
+      title: 'Reactions',
+      cap: 'All of the chat reactions that I witnessed regarding my installation.'
+    },
+  ],
+  [
+    {
+      img: 'hole_banner.jpg',
+      size: '1920x707',
+      title: 'The Hole',
+      cap: 'A perfect hole reaching bedrock in a nearby river.'
+    },
+  ],
+  [
+    {
+      img: 'hole_before2.jpg',
+      size: '1920x1080',
+      title: 'Digging',
+      cap: 'Hours went to digging this endless pit.'
+    },
+  ],
+  [
+    {
+      img: 'hole_before1.jpg',
+      size: '2000x1125',
+      title: 'Location Scouting',
+      cap: 'A river right next to the main spawn area.'
+    },
+    {
+      img: 'hole_idea.jpg',
+      size: '2000x1125',
+      title: 'Designing the Hole',
+    },
+    {
+      img: 'hole1.jpg',
+      size: '1920x1080',
+      title: 'Finished Piece',
+      cap: 'Final artwork documentation, before the public saw intervention.'
+    },
+    {
+      img: 'hole2.jpg',
+      size: '1920x1080',
+      title: 'Finished Piece',
+      cap: 'Final artwork documentation, before the public saw intervention.'
+    },
+    {
+      img: 'hole3.jpg',
+      size: '1920x1080',
+      title: 'Finished Piece',
+      cap: 'Final artwork documentation, before the public saw intervention.'
+    },
+    {
+      img: 'hole4.jpg',
+      size: '1920x1080',
+      title: 'Finished Piece',
+      cap: 'Final artwork documentation, before the public saw intervention.'
+    },
+    {
+      img: 'hole5.jpg',
+      size: '1920x1080',
+      title: 'Finished Piece',
+      cap: 'Final artwork documentation, before the public saw intervention.'
+    },
+  ],
+  [
+    {
+      img: 'hole8.jpg',
+      size: '1920x1080',
+      title: 'Visitor\'s Log',
+      cap: 'Someone created a visitor\'s log.'
+    },
+    {
+      img: 'hole7.jpg',
+      size: '1920x1080',
+      title: 'Nazi Vandalism',
+      cap: 'Nazi symbol, griefing (vandalism) and repurposing the visitor\'s log.'
+    },
+    {
+      img: 'hole6.jpg',
+      size: '1054x653',
+      title: 'Renovation',
+      cap: 'A user renovated the hole by surrounding it with land and fencing.'
+    },
+  ],
+  [
+    {
+      img: 'fountain_banner.jpg',
+      size: '1920x707',
+      title: 'The Fountain',
+      cap: 'Water falling from the heavens into a farm.'
+    },
+  ],
+  [
+    {
+      img: 'fountain2.jpg',
+      size: '1920x1080',
+      title: 'Finished Fountain',
+      cap: 'The finished fountain is 256 blocks high, in the middle of a crop field.'
+    },
+  ],
+  [
+    {
+      img: 'fountain5.jpg',
+      size: '2000x1250',
+      title: 'Location Scouting',
+      cap: 'I wanted somewhere flat but populated to build the fountain in.'
+    },
+    {
+      img: 'fountain_idea.jpg',
+      size: '2000x1250',
+      title: 'Designing the Fountain',
+      cap: 'Planning the height and construction method of the fountain.'
+    },
+    {
+      img: 'fountain1.jpg',
+      size: '2000x1250',
+      title: 'Finished Fountain',
+      cap: 'The finished fountain is 256 blocks high, in the middle of a crop field.'
+    },
+    {
+      img: 'fountain3.jpg',
+      size: '1920x1080',
+      title: 'Finished Fountain',
+      cap: 'The finished fountain is 256 blocks high, in the middle of a crop field.'
+    },
+    {
+      img: 'fountain4.jpg',
+      size: '1920x1080',
+      title: 'Finished Fountain',
+      cap: 'The finished fountain is 256 blocks high, in the middle of a crop field.'
+    },
+  ],
+];
+
+initIndiePage(img_data, 'minecraftart');
