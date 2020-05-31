07561c1b50d487bfe413748affce88e962e8f696
Hizal Celik <hizalcelik@gmail.com>
2020-05-31 13:41:15 -0400

    added hayal page

---
+++ assets/js/indie/hayal.js
@@ -0,0 +1,59 @@
+var img_data = [
+  [
+    {
+      img: 'render2.jpg',
+      size: '2000x1333',
+      title: 'World',
+      cap: 'Murky underwater world with a floating city.'
+    },
+  ],
+  [
+    {
+      img: 'render1.jpg',
+      size: '1920x1080',
+      title: 'Floating City',
+      cap: 'The ruins of an abandoned city.'
+    }
+  ],
+  [
+    {
+      img: 'loc1.jpg',
+      size: '2000x1333',
+      cap: 'Artwork on the High Line, in NYC'
+    },
+    {
+      img: 'loc2.jpg',
+      size: '2000x1333',
+      cap: 'Artwork on the High Line, in NYC'
+    },
+    {
+      img: 'setting.jpg',
+      size: '1333x2000',
+      cap: 'Artwork on the High Line, in NYC'
+    }
+  ],
+  [
+    {
+      img: 'inspo1.jpg',
+      size: '1024x552',
+    },
+    {
+      img: 'inspo2.jpg',
+      size: '1280x657',
+    },
+    {
+      img: 'inspo3.jpg',
+      size: '2000x1333',
+    },
+    {
+      img: 'inspo4.jpg',
+      size: '1920x800',
+    },
+    {
+      img: 'inspo5.jpg',
+      size: '1280x850',
+    }
+  ],
+];
+
+initIndiePage(img_data, 'hayal');
