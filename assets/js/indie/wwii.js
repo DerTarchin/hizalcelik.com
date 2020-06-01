0aeb80a65b2eb63d97cbeb1bd7635eb4fc8651f6
Hizal Celik <hizalcelik@gmail.com>
2020-05-31 21:47:26 -0400

    added wwii fighter planes page

---
+++ assets/js/indie/wwii.js
@@ -0,0 +1,117 @@
+var img_data = [
+  [
+    {
+      img: 'flying.jpg',
+      size: '2000x1333',
+    },
+  ],
+  [
+    {
+      img: 'plane1.jpg',
+      size: '2000x1123',
+      title: 'Focke Wulf FW 190'
+    },
+    {
+      img: 'plane2.jpg',
+      size: '2000x1124',
+      title: 'Hawker Hurricane'
+    },
+    {
+      img: 'plane3.jpg',
+      size: '760x570',
+      title: 'Messerschmitt Bf 109'
+    },
+    {
+      img: 'plane4.jpg',
+      size: '560x424',
+      title: 'B-17 bomber'
+    },
+    {
+      img: 'plane5.jpg',
+      size: '479x599',
+      title: 'B-24 bomber'
+    },
+    {
+      img: 'plane6.jpg',
+      size: '1500x1147',
+      title: 'Bell P-39 Airacobra'
+    },
+    {
+      img: 'plane7.jpg',
+      size: '580x430',
+      title: 'Curtiss P-40 Warhawk'
+    },
+    {
+      img: 'plane8.jpg',
+      size: '740x620',
+      title: 'Grumman F4F-3 Wildcat'
+    },
+    {
+      img: 'plane9.jpg',
+      size: '900x550',
+      title: 'Grumman F6F-5 Hellcat'
+    },
+    {
+      img: 'plane10.jpg',
+      size: '1003x388',
+      title: 'Kawanishi N1K'
+    },
+    {
+      img: 'plane11.jpg',
+      size: '941x519',
+      title: 'Lavochkin La-7S'
+    },
+    {
+      img: 'plane12.jpg',
+      size: '750x400',
+      title: 'Lockheed P-38F Lightning'
+    },
+    {
+      img: 'plane13.jpg',
+      size: '576x417',
+      title: 'Mitsubishi A6M Zero'
+    },
+    {
+      img: 'plane14.jpg',
+      size: '518x348',
+      title: 'Nakajima Ki-27'
+    },
+    {
+      img: 'plane15.jpg',
+      size: '800x353',
+      title: 'Nakajima Ki-43'
+    },
+    {
+      img: 'plane16.jpg',
+      size: '800x531',
+      title: 'Nakajima Ki-84'
+    },
+    {
+      img: 'plane17.jpg',
+      size: '600x317',
+      title: 'Navy Corsair'
+    },
+    {
+      img: 'plane18.jpg',
+      size: '1024x813',
+      title: 'P-47 Thunderbolt'
+    },
+    {
+      img: 'plane19.jpg',
+      size: '600x373',
+      title: 'P-51 Mustang'
+    },
+    {
+      img: 'plane20.jpg',
+      size: '770x532',
+      title: 'Supermarine Spitfire'
+    },
+    {
+      img: 'plane21.jpg',
+      size: '637x342',
+      title: 'Yakovlev Yak-3'
+    },
+  ],
+];
+
+initIndiePage(img_data, 'wwii');
