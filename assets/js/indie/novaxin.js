2d7e15adbda0e9d130df5455fc3c6d628fa3ea17
Hizal Celik <hizalcelik@gmail.com>
2020-07-07 08:29:18 -0400

    adds novaxin page

---
+++ assets/js/indie/novaxin.js
@@ -0,0 +1,19 @@
+var img_data = [
+  [
+    {
+      img: 'pharma.jpg',
+      size: '1270x712',
+      cap: 'Satirizing the stock footage in pharmaceutical ads.'
+    },
+  ],
+  [
+    {
+      img: 'law.jpg',
+      size: '1272x710',
+      title: 'Superman',
+      cap: 'Satirizing the side effects targetted by law firms.'
+    },
+  ],
+];
+
+initIndiePage(img_data, 'novaxin');
