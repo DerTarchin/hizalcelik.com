1ac5c578bf0737f59038fc52c83d69fd3714f70e
Hizal Celik <hizalcelik@gmail.com>
2020-08-06 09:19:20 -0400

    added 70s page

---
+++ assets/js/indie/70s.js
@@ -0,0 +1,55 @@
+var img_data = [
+  [
+    {
+      img: 'compare.gif',
+      size: '600x169',
+      title: 'Original vs Edited'
+    }
+  ],
+  [
+    {
+      img: 'timeline.jpg',
+      size: '1610x888',
+      title: 'Audio Editing'
+    }
+  ],
+  [
+    {
+      img: '70s_2.jpeg',
+      size: '2000x1123',
+      title: 'Behind the scenes'
+    },
+    {
+      img: '70s_5.jpeg',
+      size: '2000x1123',
+      title: 'Behind the scenes'
+    },
+    {
+      img: '70s_6.jpeg',
+      size: '2000x1123',
+      title: 'Behind the scenes'
+    },
+    {
+      img: '70s_7.jpeg',
+      size: '1333x2000',
+      title: 'Behind the scenes'
+    },
+    {
+      img: '70s_8.jpeg',
+      size: '1333x2000',
+      title: 'Behind the scenes'
+    },
+    {
+      img: '70s_9.jpeg',
+      size: '2000x1123',
+      title: 'Behind the scenes'
+    },
+    {
+      img: '70s_10.jpeg',
+      size: '2000x1123',
+      title: 'Behind the scenes'
+    },
+  ],
+];
+
+initIndiePage(img_data, '70s');
