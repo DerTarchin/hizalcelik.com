715abdcbe16683ea41d67e8904b997d8ec07fd5e
Hizal Celik <hizalcelik@gmail.com>
2020-10-26 23:12:16 -0400

    Added small metals page

---
+++ assets/js/indie/metals.js
@@ -0,0 +1,74 @@
+var img_data = [
+  [
+    {
+      img: 'chamber1.jpeg',
+      size: '2000x1500',
+      title: 'Metal Chamber',
+      cap: '(sealed, final form), 2015'
+    },
+    {
+      img: 'chamber2.jpeg',
+      size: '2000x1500',
+      title: 'Metal Chamber',
+      cap: '(sealed, final form), 2015'
+    },
+    {
+      img: 'chamber_bts3.jpeg',
+      size: '2000x1500',
+      title: 'Metal Chamber',
+      cap: 'Soldering sides'
+    },
+    {
+      img: 'chamber_bts2.jpeg',
+      size: '2000x1500',
+      title: 'Metal Chamber',
+      cap: 'Soldering sides'
+    },
+    {
+      img: 'chamber_bts1.jpeg',
+      size: '2000x1500',
+      title: 'Metal Chamber',
+      cap: 'Sealing the two sides with a mystery item inside'
+    },
+  ],
+  [
+    {
+      img: 'selfie.jpg',
+      size: '2000x1000',
+      title: 'Selfie Badge',
+      cap: '(final form), 2015'
+    },
+    {
+      img: 'selfie_bts.jpeg',
+      size: '1500x2000',
+      title: 'Selfie Badge (reverse)',
+    }
+  ],
+  [
+    {
+      img: 'hearts3.jpeg',
+      size: '1500x2000',
+      title: 'Selfie Badge',
+      cap: 'Heart, 2015'
+    },
+    {
+      img: 'hearts4.jpeg',
+      size: '2000x1500',
+      title: 'Heart Necklace, 2015',
+    },
+    {
+      img: 'hearts1.jpeg',
+      size: '2000x1500',
+      title: 'Heart',
+      cap: 'Cutting and creating the heart'
+    },
+    {
+      img: 'hearts2.jpeg',
+      size: '1500x2000',
+      title: 'Heart Necklace',
+      cap: 'Cutting and creating the heart necklace'
+    }
+  ]
+];
+
+initIndiePage(img_data, 'metals');
