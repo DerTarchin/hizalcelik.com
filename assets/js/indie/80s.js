3e4f7d65cb8e7eb47eeb2859fcc868f953e4aa5c
Hizal Celik <hizalcelik@gmail.com>
2020-10-30 20:37:50 -0400

    Added 80s page

---
+++ assets/js/indie/80s.js
@@ -0,0 +1,45 @@
+var img_data = [
+  [
+    {
+      img: 'production_2.jpeg',
+      size: '1500x2000',
+      title: 'Blacklight Tests',
+    },
+  ],
+  [
+    {
+      img: 'production_5.jpeg',
+      size: '2000x1333',
+      title: 'Blacklight Tests',
+    },
+  ],
+  [
+    {
+      img: 'production_3.jpeg',
+      size: '1500x2000',
+      title: 'Blacklight Tests',
+    },
+    {
+      img: 'production_6.jpeg',
+      size: '2000x1333',
+      title: 'Blacklight Tests',
+    },
+    {
+      img: 'production_7.jpeg',
+      size: '1333x2000',
+      title: 'Blacklight Tests',
+    },
+    {
+      img: 'production_8.jpeg',
+      size: '2000x1333',
+      title: 'Blacklight Tests',
+    },
+    {
+      img: 'production_9.jpeg',
+      size: '2000x1333',
+      title: 'Blacklight Tests',
+    },
+  ],
+];
+
+initIndiePage(img_data, '80s');
