6188eb3b5ca534e3913bf483d81a0a86757fb7dd
Hizal Celik <hizalcelik@gmail.com>
2020-10-31 00:08:50 -0400

    Adds abstract page

---
+++ assets/js/indie/abstract.js
@@ -0,0 +1,144 @@
+var img_data = [
+  [
+    {
+      img: 'bts.jpeg',
+      size: '2000x1070',
+      title: 'Shooting footage in the studio',
+    },
+  ],
+  [
+    {
+      img: 'i7.jpg',
+      size: '2000x833',
+      title: 'Part I',
+    },
+  ],
+  [
+    {
+      img: 'i1.jpg',
+      size: '2000x833',
+      title: 'Part I',
+    },
+    {
+      img: 'i2.jpg',
+      size: '2000x833',
+      title: 'Part I',
+    },
+    {
+      img: 'i3.jpg',
+      size: '2000x833',
+      title: 'Part I',
+    },
+    {
+      img: 'i4.jpg',
+      size: '2000x833',
+      title: 'Part I',
+    },
+    {
+      img: 'i5.jpg',
+      size: '2000x833',
+      title: 'Part I',
+    },
+    {
+      img: 'i6.jpg',
+      size: '2000x833',
+      title: 'Part I',
+    },
+    {
+      img: 'i8.jpg',
+      size: '2000x833',
+      title: 'Part I',
+    },
+  ],
+  [
+    {
+      img: 'ii4.jpg',
+      size: '2000x833',
+      title: 'Part II',
+    },
+  ],
+  [
+    {
+      img: 'ii1.jpg',
+      size: '2000x833',
+      title: 'Part II',
+    },
+    {
+      img: 'ii2.jpg',
+      size: '2000x833',
+      title: 'Part II',
+    },
+    {
+      img: 'ii3.jpg',
+      size: '2000x833',
+      title: 'Part II',
+    },
+    {
+      img: 'ii5.jpg',
+      size: '2000x833',
+      title: 'Part II',
+    },
+    {
+      img: 'ii6.jpg',
+      size: '2000x833',
+      title: 'Part II',
+    },
+    {
+      img: 'ii7.jpg',
+      size: '2000x833',
+      title: 'Part II',
+    },
+    {
+      img: 'ii8.jpg',
+      size: '2000x833',
+      title: 'Part II',
+    },
+    {
+      img: 'ii9.jpg',
+      size: '2000x833',
+      title: 'Part II',
+    },
+    {
+      img: 'ii10.jpg',
+      size: '2000x833',
+      title: 'Part II',
+    },
+    {
+      img: 'ii11.jpg',
+      size: '2000x833',
+      title: 'Part II',
+    },
+    {
+      img: 'ii12.jpg',
+      size: '2000x833',
+      title: 'Part II',
+    },
+    {
+      img: 'ii13.jpg',
+      size: '2000x833',
+      title: 'Part II',
+    },
+    {
+      img: 'ii14.jpg',
+      size: '2000x833',
+      title: 'Part II',
+    },
+    {
+      img: 'ii15.jpg',
+      size: '2000x833',
+      title: 'Part II',
+    },
+    {
+      img: 'ii16.jpg',
+      size: '2000x833',
+      title: 'Part II',
+    },
+    {
+      img: 'ii17.jpg',
+      size: '2000x833',
+      title: 'Part II',
+    },
+  ],
+];
+
+initIndiePage(img_data, 'abstract');
