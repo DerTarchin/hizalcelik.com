e39eca82945bc6d181943d13d4ddef4edc741698
Hizal Celik <hizalcelik@gmail.com>
2020-11-01 20:03:42 -0500

    Adds shapes page

---
+++ assets/js/indie/shapes.js
@@ -0,0 +1,33 @@
+var img_data = [
+  [
+    {
+      img: 'smile.gif',
+      size: '323x323',
+      title: 'Animation Experiment',
+    },
+  ],
+  [
+    {
+      img: 'shapes1.jpeg',
+      size: '2000x1500',
+      title: 'Cutting paper into small cards',
+    },
+    {
+      img: 'shapes2.jpeg',
+      size: '2000x1488',
+      title: 'Drawing using lightbox',
+    },
+    {
+      img: 'shapes3.jpeg',
+      size: '1488x2000',
+      title: 'Drawing using lightbox',
+    },
+    {
+      img: 'shapes4.jpeg',
+      size: '1488x2000',
+      title: 'Stacks of the final drawn frames',
+    },
+  ]
+];
+
+initIndiePage(img_data, 'shapes');
