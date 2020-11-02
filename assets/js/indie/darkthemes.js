9aba91b5cbb65654ddba5399efa4b60c41fe8179
Hizal Celik <hizalcelik@gmail.com>
2020-11-01 23:19:26 -0500

    added originals to darkthemes

--- assets/js/indie/darkthemes.js
+++ assets/js/indie/darkthemes.js
@@ -1,17 +1,27 @@
 var img_data = [
   [
     {
+      img: 'tv_white.jpg',
+      size: '2000x1250',
+      title: 'TradingView (original)',
+    },
+    {
       img: 'tradingview1.jpg',
       size: '2000x1250',
       title: 'TradingView CSS Dark Theme',
     },
-    {
-      img: 'tradingview2.jpg',
-      size: '2000x1250',
-      title: 'TradingView CSS Dark Theme',
-    },
+    // {
+    //   img: 'tradingview2.jpg',
+    //   size: '2000x1250',
+    //   title: 'TradingView CSS Dark Theme',
+    // },
   ],
   [
+    {
+      img: 'st_white.jpg',
+      size: '1685x1010',
+      title: 'Stocktwits (original)',
+    },
     {
       img: 'stocktwits.jpg',
       size: '1685x1010',
