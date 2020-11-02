718a522d1056b8f328ab4134af871539897ee6c4
Hizal Celik <hizalcelik@gmail.com>
2020-11-01 19:19:47 -0500

    Added carrig page

---
+++ assets/js/indie/carrig.js
@@ -0,0 +1,16 @@
+var img_data = [
+  [
+    {
+      img: 'carrig2.jpg',
+      size: '1024x436',
+    },
+  ],
+  [
+    {
+      img: 'carrig.gif',
+      size: '640x480',
+    },
+  ]
+];
+
+initIndiePage(img_data, 'carrig');
