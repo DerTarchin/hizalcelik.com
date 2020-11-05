1170343fe1823b5dd41db770e380cf96aab454c7
Hizal Celik <hizalcelik@gmail.com>
2020-11-05 13:06:36 -0500

    Added attic page

---
+++ assets/js/indie/attic.js
@@ -0,0 +1,330 @@
+var img_data = [
+  [
+    {
+      img: 'attic25.jpg',
+      size: '1280x720',
+      title: 'Attic',
+      cap: '2013'
+    },
+    {
+      img: 'attic26.jpg',
+      size: '1280x720',
+      title: 'Attic',
+      cap: '2013'
+    },
+  ],
+  [
+    {
+      img: 'inspo_attic_1.jpeg',
+      size: '500x375',
+      title: 'Attic Inspiration',
+    },
+    {
+      img: 'inspo_attic_2.jpg',
+      size: '640x480',
+      title: 'Attic Inspiration',
+    },
+    {
+      img: 'inspo_attic_3.jpeg',
+      size: '500x335',
+      title: 'Attic Inspiration',
+    },
+    {
+      img: 'inspo_attic_4.jpeg',
+      size: '251x249',
+      title: 'Attic Inspiration',
+    },
+    {
+      img: 'inspo_attic_5.jpg',
+      size: '500x333',
+      title: 'Attic Inspiration',
+    },
+    {
+      img: 'inspo_attic_6.jpg',
+      size: '682x785',
+      title: 'Attic Inspiration',
+    },
+    {
+      img: 'inspo_attic_7.jpg',
+      size: '2000x1250',
+      title: 'Attic Inspiration',
+    },
+  ],
+  [
+    {
+      img: 'screen.jpg',
+      size: '1922x1202',
+      title: 'Production Screenshots of 3ds Max Workflow',
+    },
+    {
+      img: 'screen2.jpg',
+      size: '1922x1202',
+      title: 'Production Screenshots of 3ds Max Workflow',
+    },
+    {
+      img: 'screen3.jpg',
+      size: '1922x1202',
+      title: 'Production Screenshots of 3ds Max Workflow',
+    },
+    {
+      img: 'attic1.jpg',
+      size: '1280x720',
+      title: 'Creating the House',
+    },
+    {
+      img: 'attic2.jpg',
+      size: '1280x720',
+      title: 'Creating the House',
+    },
+    {
+      img: 'attic3.jpg',
+      size: '1280x720',
+      title: 'Creating the House',
+    },
+    {
+      img: 'attic4.jpg',
+      size: '1280x720',
+      title: 'Creating the House',
+    },
+    {
+      img: 'attic5.jpg',
+      size: '1280x720',
+      title: 'Creating the House',
+    },
+    {
+      img: 'attic6.jpg',
+      size: '1024x436',
+      title: 'Creating the House',
+    },
+    {
+      img: 'attic7.jpg',
+      size: '1280x720',
+      title: 'Creating the House',
+    },
+    {
+      img: 'attic8.jpg',
+      size: '1280x720',
+      title: 'Creating the House',
+    },
+    {
+      img: 'attic9.jpg',
+      size: '1280x720',
+      title: 'Creating the House',
+    },
+    {
+      img: 'attic10.jpg',
+      size: '1280x720',
+      title: 'Creating the House',
+    },
+    {
+      img: 'attic11.jpg',
+      size: '480x270',
+      title: 'Creating the House',
+    },
+    {
+      img: 'attic12.jpg',
+      size: '1280x720',
+      title: 'Creating the House',
+    },
+    {
+      img: 'attic13.jpg',
+      size: '1280x720',
+      title: 'Creating the House',
+    },
+    {
+      img: 'attic14.jpg',
+      size: '480x270',
+      title: 'Creating the House',
+    },
+    {
+      img: 'attic15.jpg',
+      size: '480x270',
+      title: 'Creating the House',
+    },
+    {
+      img: 'attic16.jpg',
+      size: '480x270',
+      title: 'Creating the House',
+    },
+    {
+      img: 'attic17.jpg',
+      size: '480x270',
+      title: 'Creating the House',
+    },
+    {
+      img: 'attic18.jpg',
+      size: '480x270',
+      title: 'Creating the House',
+    },
+    {
+      img: 'attic19.jpg',
+      size: '1280x720',
+      title: 'Creating the House',
+    },
+    {
+      img: 'attic20.jpg',
+      size: '1280x720',
+      title: 'Creating the House',
+    },
+    {
+      img: 'attic21.jpg',
+      size: '1280x720',
+      title: 'Creating the House',
+    },
+    {
+      img: 'attic22.jpg',
+      size: '480x270',
+      title: 'Creating the House',
+    },
+    {
+      img: 'attic23.jpg',
+      size: '480x270',
+      title: 'Creating the House',
+    },
+    {
+      img: 'attic24.jpg',
+      size: '1280x720',
+      title: 'Creating the House',
+    },
+    {
+      img: 'attic25.jpg',
+      size: '1280x720',
+      title: 'Creating the House',
+    },
+    {
+      img: 'attic26.jpg',
+      size: '1280x720',
+      title: 'Creating the House',
+    },
+    {
+      img: 'attic27.jpg',
+      size: '1024x436',
+      title: 'Creating the House',
+    },
+  ],
+  [
+    {
+      img: 'inspo_house_1.jpeg',
+      size: '600x336',
+      title: 'House Inspiration',
+    },
+    {
+      img: 'inspo_house_2.jpeg',
+      size: '1020x765',
+      title: 'House Inspiration',
+    },
+    {
+      img: 'inspo_house_3.jpg',
+      size: '2000x1250',
+      title: 'House Inspiration',
+    },
+    {
+      img: 'inspo_house_4.jpg',
+      size: '2000x1250',
+      title: 'House Inspiration',
+    },
+    {
+      img: 'inspo_house_5.jpeg',
+      size: '509x381',
+      title: 'House Inspiration',
+    },
+    {
+      img: 'inspo_house_6.jpeg',
+      size: '640x479',
+      title: 'House Inspiration',
+    },
+  ],
+  [
+    {
+      img: 'house1.jpg',
+      size: '640x480',
+      title: 'Creating the House',
+    },
+    {
+      img: 'house2.jpg',
+      size: '640x480',
+      title: 'Creating the House',
+    },
+    {
+      img: 'house3.jpg',
+      size: '640x480',
+      title: 'Creating the House',
+    },
+    {
+      img: 'house4.jpg',
+      size: '640x480',
+      title: 'Creating the House',
+    },
+    {
+      img: 'house5.jpg',
+      size: '640x480',
+      title: 'Creating the House',
+    },
+  ],
+  [
+    {
+      img: 'Test_Bot.jpg',
+      size: '1280x720',
+      title: 'Robot Character (WIP)',
+    }
+  ],
+  [
+    {
+      img: 'inspo_robo_1.jpeg',
+      size: '1000x800',
+      title: 'Character Inspiration',
+    },
+    {
+      img: 'inspo_robo_2.jpeg',
+      size: '267x400',
+      title: 'Character Inspiration',
+    },
+    {
+      img: 'inspo_robo_3.jpeg',
+      size: '273x400',
+      title: 'Character Inspiration',
+    },
+    {
+      img: 'inspo_robo_4.jpeg',
+      size: '1031x951',
+      title: 'Character Inspiration',
+    },
+    {
+      img: 'inspo_robo_5.jpeg',
+      size: '320x475',
+      title: 'Character Inspiration',
+    },
+    {
+      img: 'inspo_robo_6.jpeg',
+      size: '1116x1800',
+      title: 'Character Inspiration',
+    },
+    {
+      img: 'inspo_robo_7.jpeg',
+      size: '300x220',
+      title: 'Character Inspiration',
+    },
+    {
+      img: 'inspo_robo_8.jpeg',
+      size: '1297x2000',
+      title: 'Character Inspiration',
+    },
+    {
+      img: 'inspo_robo_9.jpeg',
+      size: '451x600',
+      title: 'Character Inspiration',
+    },
+    {
+      img: 'inspo_robo_10.jpeg',
+      size: '500x333',
+      title: 'Character Inspiration',
+    },
+    {
+      img: 'inspo_robo_11.jpeg',
+      size: '450x383',
+      title: 'Character Inspiration',
+    },
+  ]
+];
+
+initIndiePage(img_data, 'attic');
