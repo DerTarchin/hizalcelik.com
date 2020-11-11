48a8b16ca1528f54e1481738897528b1f8bedc5e
Hizal Celik <hizalcelik@gmail.com>
2020-11-11 08:19:49 -0500

    Adds fishbowl page

---
+++ assets/js/indie/fishbowl.js
@@ -0,0 +1,21 @@
+var img_data = [
+  [
+    {
+      img: 'fish1.jpg',
+      size: '639x642',
+      title: 'Texture mapping',
+    },
+    {
+      img: 'fish2.jpg',
+      size: '1011x564',
+      title: 'Fish',
+    },
+    {
+      img: 'fish3.jpg',
+      size: '1026x578',
+      title: 'Fish',
+    },
+  ],
+];
+
+initIndiePage(img_data, 'fishbowl');
