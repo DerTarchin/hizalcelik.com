87821aac171bd5a19ef2e1d601ef481510914e42
Hizal Celik <hizalcelik@gmail.com>
2020-12-04 07:34:10 -0500

    Adds laptop photogrammetry page

---
+++ assets/js/indie/laptop.js
@@ -0,0 +1,12 @@
+var img_data = [
+  [
+    {
+      img: 'photogrammetry.gif',
+      size: '994x458',
+      title: 'My Laptop Scanned',
+      cap: '3D point model using photogrammetry'
+    },
+  ],
+];
+
+initIndiePage(img_data, 'laptop');
