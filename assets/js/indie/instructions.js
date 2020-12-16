066fcff8863505cc8e3ac927c5d22c1815707aa4
Hizal Celik <hizalcelik@gmail.com>
2020-12-16 08:26:25 -0500

    added instruction page

---
+++ assets/js/indie/instructions.js
@@ -0,0 +1,24 @@
+var img_data = [
+  [
+    {
+      img: 'game1.jpg',
+      size: '1376x2000',
+      title: 'Submission to Instruction Assignment',
+      cap: 'One volunteer attempted to follow the instructions to create this'
+    },
+    {
+      img: 'game2.jpg',
+      size: '1490x2000',
+      title: 'Submission to Instruction Assignment',
+      cap: 'One volunteer attempted to follow the instructions to create this'
+    },
+    {
+      img: 'game3.jpg',
+      size: '1391x2000',
+      title: 'Submission to Instruction Assignment',
+      cap: 'One volunteer attempted to follow the instructions to create this'
+    }
+  ]
+]
+
+initIndiePage(img_data, 'instructions');
