faa5dccc36e640e3a35f248d6c2b6c781ebe1e08
Hizal Celik <hizalcelik@gmail.com>
2020-12-16 19:12:27 -0500

    Added satstudy page

---
+++ assets/js/indie/satstudy.js
@@ -0,0 +1,16 @@
+var img_data = [
+  [
+    {
+      img: 'color.jpg',
+      size: '2000x1500',
+      title: 'Full color screengrab',
+    },
+    {
+      img: 'bw.jpg',
+      size: '2000x1500',
+      title: 'Full color screengrab converted to black and white to show the range in value (grays)',
+    },
+  ],
+];
+
+initIndiePage(img_data, 'satstudy');
