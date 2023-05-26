39b93b20b8a016ab6858f41925cf18411b7f9d70
Hizal <hizal@grata.com>
2023-05-26 08:33:39 -0400

    add more studies and sketches

---
+++ assets/js/indie/sketches.js
@@ -0,0 +1,92 @@
+var img_data = [
+  [
+    {
+      img: 'arch.jpg',
+      size: '2000x1501',
+      title: 'Arch',
+      cap: 'Pencil drawing on paper, 2014'
+    },
+    {
+      img: 'nicole.jpg',
+      size: '1500x2000',
+      title: 'Portrait',
+      cap: 'Pencil drawing on paper, 2014'
+    },
+    {
+      img: 'basement.jpg',
+      size: '2000x1442',
+      title: 'Room',
+      cap: 'Pencil drawing on paper, 2014'
+    },
+    {
+      img: 'building.jpg',
+      size: '2000x1500',
+      title: 'Building',
+      cap: 'Pencil drawing on paper, 2014'
+    },
+    {
+      img: 'darkness.jpg',
+      size: '2000x1500',
+      title: 'Darkness',
+      cap: 'Ink drawing on paper, 2014'
+    },
+    {
+      img: 'home.jpg',
+      size: '2000x1500',
+      title: 'Home',
+      cap: 'Pencil drawing on paper, 2014'
+    },
+    {
+      img: 'ink.jpg',
+      size: '2000x1508',
+      title: 'Ink Sketches',
+      cap: 'Ink drawing on paper, 2014'
+    },
+    {
+      img: 'thecut.jpg',
+      size: '2000x1500',
+      title: 'The Cut',
+      cap: 'Charcoal drawing on paper, 2014'
+    },
+  ],
+  [
+    {
+      img: 'painting_studies1.JPG',
+      size: '1333x2000',
+      title: 'Paint Studies',
+      cap: 'Ink drawing on paper, 2015'
+    },
+    {
+      img: 'painting_studies2.JPG',
+      size: '1333x2000',
+      title: 'Paint Studies',
+      cap: 'Ink drawing on paper, 2015'
+    },
+    {
+      img: 'painting_studies3.JPG',
+      size: '1333x2000',
+      title: 'Paint Studies',
+      cap: 'Ink drawing on paper, 2015'
+    },
+    {
+      img: 'painting_studies4.JPG',
+      size: '1333x2000',
+      title: 'Paint Studies',
+      cap: 'Ink drawing on paper, 2015'
+    },
+    {
+      img: 'painting_studies5.JPG',
+      size: '1333x2000',
+      title: 'Paint Studies',
+      cap: 'Ink drawing on paper, 2015'
+    },
+    {
+      img: 'painting_studies6.JPG',
+      size: '1333x2000',
+      title: 'Paint Studies',
+      cap: 'Ink drawing on paper, 2015'
+    },
+  ]
+];
+
+initIndiePage(img_data, 'sketches');
