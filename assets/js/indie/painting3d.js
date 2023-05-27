12ccfa40435c0f2a0eba8a5749e24538e556a0e9
Hizal <hizal@grata.com>
2023-05-27 09:05:59 -0400

    Added painting page

---
+++ assets/js/indie/painting3d.js
@@ -0,0 +1,26 @@
+var img_data = [
+  [
+    {
+      img: 'paintin3d.JPG',
+      size: '2000x1333',
+      title: 'Girl in Tree',
+      cap: 'Carpet, beads, filing cabinet paneling, newspaper, shells, misc. materials. 12"x12", 2015'
+    }
+],
+[
+    {
+      img: 'vectorized.jpg',
+      size: '2000x1125',
+      title: 'Digital sketch',
+      cap: 'Vectorized sketch'
+    },
+    {
+      img: 'vectorized_nude.jpg',
+      size: '2000x1125',
+      title: 'Digital sketch, nude',
+      cap: 'Vectorized sketch, nude'
+    }
+  ]
+]
+
+initIndiePage(img_data, 'painting3d');
